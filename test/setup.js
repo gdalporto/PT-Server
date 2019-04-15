df736058a0e02ac95b97c72c671f7c7ff295637a
Gabriel Dalporto <dalporto@yahoo.com>
2019-04-15 11:36:34 -0700

    cleaned up package jason

--- test/setup.js
+++ test/setup.js
@@ -3,8 +3,8 @@ const chaiHttp = require('chai-http');
 global.expect = chai.expect;
 chai.use(chaiHttp);
 
-process.env.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-mlc-app'; //insert test database url
-process.env.DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-mlc-app'; //insert test database url
+// process.env.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-mlc-app'; //insert test database url
+// process.env.DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-mlc-app'; //insert test database url
 process.env.JWT_SECRET = "testJWTsecret";
 
 
