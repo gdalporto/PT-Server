ae76b44b90e8d3ce1fbabe76c7ecc1b5fbb971aa
Gabriel Dalporto <dalporto@yahoo.com>
2019-04-15 19:11:55 -0700

    fixing mlab ref

--- test/setup.js
+++ test/setup.js
@@ -3,7 +3,7 @@ const chaiHttp = require('chai-http');
 global.expect = chai.expect;
 chai.use(chaiHttp);
 
-process.env.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/PT-test'; //insert test database url
+process.env.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://dalporto:Hello12345@ds239936.mlab.com:39936/pt-database-test' || 'mongodb://localhost/PT-test'; //insert test database url
 process.env.JWT_SECRET = "testJWTsecret";
 
 
